b85b0468ae9fa32bb9e1dd1633851425ced3f690
Srisanthosh19 <140731958+Srisanthosh19@users.noreply.github.com>
2024-10-06 22:32:11 +0530

    intial commit

---
+++ public/src/components/Experience.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import { motion } from "framer-motion";
+
+import "react-vertical-timeline-component/style.min.css";
+
+import { styles } from "../styles";
+import { experiences } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { textVariant } from "../utils/motion";
+
+const ExperienceCard = ({ experience }) => {
+  return (
+    <VerticalTimelineElement
+      contentStyle={{
+        background: "#1d1836",
+        color: "#fff",
+      }}
+      contentArrowStyle={{ borderRight: "7px solid  #232631" }}
+      date={experience.date}
+      iconStyle={{ background: experience.iconBg }}
+      icon={
+        <div className='flex justify-center items-center w-full h-full'>
+          <img
+            src={experience.icon}
+            alt={experience.company_name}
+            className='w-[60%] h-[60%] object-contain'
+          />
+        </div>
+      }
+    >
+      <div>
+        <h3 className='text-white text-[24px] font-bold'>{experience.title}</h3>
+        <p
+          className='text-secondary text-[16px] font-semibold'
+          style={{ margin: 0 }}
+        >
+          {experience.company_name}
+        </p>
+      </div>
+
+      <ul className='mt-5 list-disc ml-5 space-y-2'>
+        {experience.points.map((point, index) => (
+          <li
+            key={`experience-point-${index}`}
+            className='text-white-100 text-[14px] pl-1 tracking-wider'
+          >
+            {point}
+          </li>
+        ))}
+      </ul>
+    </VerticalTimelineElement>
+  );
+};
+
+const Experience = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={`${styles.sectionSubText}`}>
+          What I have done so far
+        </p>
+        <h2 className={`${styles.sectionHeadText}`}>
+          Work Experience.
+        </h2>
+        <motion.p
+          className={`mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]`}
+        >
+
+        This portfolio highlights the individual’s academic background and specialization, showcasing my skills and experience. It emphasizes my journey of continuous learning and skill development. The portfolio serves as a reflection of my dedication to growing in my profession and  also applying my knowledge in practical, real-world scenarios.
+        </motion.p>
+      </motion.div>
+      {/* <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+      Srisanthosh is an undergraduate student specializing in Artificial Intelligence and Data Science at Nandha Engineering College. With a solid foundation in Python and experience in AI, he is actively developing skills for a career in data-driven technology.
+      </motion.p> */}
+
+      {/* <div className='mt-20 flex flex-col'>
+      
+        <VerticalTimeline>
+          {experiences.map((experience, index) => (
+            <ExperienceCard
+              key={`experience-${index}`}
+              experience={experience}
+            />
+          ))}
+        </VerticalTimeline>
+      </div> */}
+    </>
+  );
+};
+
+export default SectionWrapper(Experience, "work");
